2141c5695234e8d338805f0a6f17c3bbc6baa922
ajidwisaputra27 <ajidwisaputra067@gmail>
2023-01-29 17:15:45 +0700

    fix: update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+  env: {
+    NEXT_MODE: process.env.NEXT_MODE,
+  },
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
