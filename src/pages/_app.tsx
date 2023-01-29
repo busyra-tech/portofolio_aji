9b131dd61f527a1e2c4879ebfa6a42ab6afb9ddf
ajidwisaputra27 <ajidwisaputra067@gmail>
2023-01-29 16:58:58 +0700

    fix: update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,17 @@ import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
 	// return <Component {...pageProps} />
 	return (
 		<ThemeProvider attribute="class">
+			<Head>
+				<title>Aji Dwi Saputra</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
 			<div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
 				{/* // do this div style later (after putting the content) */}
 				<div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,9 +9,6 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
 					rel="stylesheet"
 				/>
-				<title>Aji Dwi Saputra</title>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
 			<body className="bg-fixed bg-gradient-to-r from-white to-white-700 dark:from-dark-500 dark:to-dark-700 dark:text-white">
 				<Main />
