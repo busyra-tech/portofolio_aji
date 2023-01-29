f4e2e0155f8e2c091c7fd3eaa118612cec50290c
ajidwisaputra27 <ajidwisaputra067@gmail>
2023-01-29 17:19:52 +0700

    git push

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-  env: {
-    NEXT_MODE: process.env.NEXT_MODE,
-  },
+	// reactStrictMode: true,
+	env: {
+		NEXT_MODE: process.env.NEXT_MODE,
+	},
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
